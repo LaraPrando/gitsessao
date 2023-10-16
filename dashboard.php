25365a8f217d53011e97669acfe5322d05b558cd
LaraPrando <laramoura070506@gmail.com>
2023-10-16 14:26:59 -0300

    commit: Atualização da pagina dashboard (php)

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+if (!isset($_SESSION["usuario"])) {
+    header("Location: login.php");
+    exit;
+}
+echo "Bem-vindo, " . $_SESSION["usuario"] . "! Esta é a pagina de dashboard."; 
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
@@ -6,6 +14,6 @@
     <title>Dashboard</title>
 </head>
 <body>
-    <h1> Dashboard<h1>
+    <a href="logout.php">Sair </a>
 </body>
 </html>
